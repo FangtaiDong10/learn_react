e67917c4f624f9eb9e843760e8636d7bbd3e5f13
FangtaiDong10 <fangtaidong10@gmail.com>
2022-12-03 22:33:21 +1100

    newExpense and ExpenseForm components update

---
+++ src/components/NewExpense/NewExpense.js
@@ -0,0 +1,15 @@
+import React from "react";
+import "./NewExpense.css";
+
+import ExpenseForm from "./ExpenseForm";
+
+//  return a form with user input
+const NewExpense = () => {
+  return (
+    <div className="new-expense">
+      <ExpenseForm />
+    </div>
+  );
+};
+
+export default NewExpense;
